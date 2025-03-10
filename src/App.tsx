93f114146db24e2c48cd92105fe39cd5e75a6002
Swyzsh <swyzsh@protonmail.com>
2025-03-11 00:30:31 +0530

    init ascii

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import ASCIIText from "@/components/ascii-text";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <main className="flex min-h-svh flex-col items-stretch justify-start">
+      <div className="relative h-[448px] w-svw bg-transparent max-md:h-[224px]">
+        <ASCIIText
+          text="Vault"
+          asciiFontSize={4}
+          textFontSize={200}
+          textColor={"#fdf9f3"}
+          planeBaseHeight={8}
+          enableWaves={true}
+        />
+      </div>
+    </main>
   );
 }
 
